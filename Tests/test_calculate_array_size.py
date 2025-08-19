c59f570c4882250d61d7b7e28468869534de8be2
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-08-19 18:23:59 +0200

    Write tests for the Calculate array size function, test driver development

---
+++ Tests/test_calculate_array_size.py
@@ -0,0 +1,28 @@
+from Modules.adjoint_pattern_database_module import calculate_array_size
+import pytest
+
+def test_calculate_array_size():
+    assert calculate_array_size(9, 4) == 15120
+    assert calculate_array_size(9, 3) == 3024
+    assert calculate_array_size(9, 2) == 504
+    assert calculate_array_size(9, 1) == 72
+    assert calculate_array_size(9, 0) == 9
+
+def test_invalid_input():
+    with pytest.raises(TypeError):
+        calculate_array_size("9", 4)
+
+    with pytest.raises(TypeError):
+        calculate_array_size(9, "4")
+
+    with pytest.raises(ValueError):
+        calculate_array_size(9, -1)
+
+    with pytest.raises(ValueError):
+        calculate_array_size(9, 10)
+
+    with pytest.raises(ValueError):
+        calculate_array_size(9, 9)
+    
+    with pytest.raises(ValueError):
+        calculate_array_size(0, -1)
