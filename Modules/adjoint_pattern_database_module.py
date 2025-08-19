8f092625c5b8ddc9d3a8cdfdf871ce7fdd39c6d0
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-08-19 18:26:32 +0200

    Finish work on the calculate_array_size function that will help me calculate the array size needed for the adjoin pattern database

---
+++ Modules/adjoint_pattern_database_module.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+
+def calculate_array_size(number_of_tiles : int, k : int) -> int:
+    if not isinstance(number_of_tiles, int) or not isinstance(k, int):
+        raise TypeError("Both arguments must be integers.")
+    if number_of_tiles < 1 or k < 0 or k >= number_of_tiles:
+        raise ValueError("Invalid input values.")
+    
+    # Calculate the size of the array needed to store the tile configurations
+    return math.prod(range(number_of_tiles, number_of_tiles - k - 1, -1))
